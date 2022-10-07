bcfcf666725e3dfbcb90204a422f50555140bfc5
Yugesh Kushwaha <96573577+yugesh01@users.noreply.github.com>
2022-10-07 22:05:02 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,39 @@
-import React from 'react'
+import React,{useState} from 'react'
 
 import '../styles/App.css';
 
 
 const searchArray = ["Newton", "School", "Newton School", "React", "Preact", "Node", "Mongoose", "Angular", "Vue", "Apple", "Microsoft", "Netflix", "Meta"]
 const App = () => {
+  const [ state,setState]=useState()
+// const {query, serchText}= useGlobleContext()  
 
   
   return (
     <div id="main">
-      
+     
+        <div>
+          <input 
+          id="search-input"
+          type="text"
+          placeholder="search text"
+          onChange={(e) => setState(e.target.value)}
+          />
+
+          {searchArray.filter((val)={
+           if(state == ""){
+            return val
+           }
+           else if(val.toLowerCase().includes(state.toLowerCase())){
+            return val
+
+           }
+           }).map((val, key)=>{
+            return<div key={key}>{val}
+              </div>
+           })}
+        </div>
+     
     </div>
   )
 }
